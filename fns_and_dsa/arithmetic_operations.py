c11581971589d6be49e93f07967b258c13d763e3
Abimbola Awopetu <126657330+Tomini1@users.noreply.github.com>
2025-06-11 15:33:10 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1: float, num2: float, operation: str ):
+def perform_operation(num1, num2, operation):
     """performs basic arithmetic operations: add, subtract, multiply, divide. """
     if operation == "add":
         return num1 + num2
