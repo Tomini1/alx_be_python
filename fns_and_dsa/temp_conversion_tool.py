d0c3869f70dfeeffc7b869525e45099f70ecc37e
Abimbola Awopetu <126657330+Tomini1@users.noreply.github.com>
2025-06-11 14:36:55 +0100

    first commit for fns_and_dsa

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,32 @@
+# Defining global conversion factors
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+
+def convert_to_celsius(fahrenheit):
+    """Converts Fahrenheit to Celsius using the global factor."""
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    """Convert Celsius to Fahrenheit using the global factor."""
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+def main():
+    try: 
+        temp = float(input("Enter the temperature to convert: "))
+        unit = input("Is this temperature in Celsius 'C' or Fahrenheit 'F' ?")
+
+        if unit == "C":
+            converted_temp = convert_to_fahrenheit(temp)
+            print(f"{temp:.1f}°C is {converted_temp:.1f}°F")
+        
+        elif unit == "F":
+            converted_temp = convert_to_celsius(temp)
+            print(f"{temp:.1f}°F is {converted_temp:.if}°C")
+        
+        else:
+            raise ValueError("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value.")
+
+if __name__ == "__main__":
+    main()
